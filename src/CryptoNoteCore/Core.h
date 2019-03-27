8a071718e2594649d38f1f72c5ece640c3de8fd7
Aiwe <maksymivv@gmail.com>
2019-03-27 02:02:27 +0200

    Added a batch of compatibility updates for Karbo 2

--- src/CryptoNoteCore/Core.h
+++ src/CryptoNoteCore/Core.h
@@ -143,15 +143,18 @@ private:
 
   void throwIfNotInitialized() const;
   bool extractTransactions(const std::vector<BinaryArray>& rawTransactions, std::vector<CachedTransaction>& transactions, uint64_t& cumulativeSize);
+  bool getMixin(const Transaction& transaction, uint64_t& mixin);
 
   std::error_code validateSemantic(const Transaction& transaction, uint64_t& fee, uint32_t blockIndex);
   std::error_code validateTransaction(const CachedTransaction& transaction, TransactionValidatorState& state, IBlockchainCache* cache, uint64_t& fee, uint32_t blockIndex);
-  
+  std::error_code validateMixin(const Transaction& transaction, uint32_t blockIndex);
+  std::error_code validateFee(const Transaction& transaction, uint32_t blockIndex);
+
   uint32_t findBlockchainSupplement(const std::vector<Crypto::Hash>& remoteBlockIds) const;
   std::vector<Crypto::Hash> getBlockHashes(uint32_t startBlockIndex, uint32_t maxCount) const;
 
   std::error_code validateBlock(const CachedBlock& block, IBlockchainCache* cache, uint64_t& minerReward);
-
+  
   uint64_t getAdjustedTime() const;
   void updateMainChainSet();
   IBlockchainCache* findSegmentContainingBlock(const Crypto::Hash& blockHash) const;
