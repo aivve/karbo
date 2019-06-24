242bc6a0b470e65a4f668773946ad8409379a4d7
Aiwe <maksymivv@gmail.com>
2019-06-24 16:43:07 +0300

    Attempt to fix difficulty check

--- src/CryptoNoteCore/DatabaseBlockchainCache.cpp
+++ src/CryptoNoteCore/DatabaseBlockchainCache.cpp
@@ -1355,7 +1355,7 @@ Difficulty DatabaseBlockchainCache::getDifficultyForNextBlock(uint32_t blockInde
   auto timestamps = getLastTimestamps(currency.difficultyBlocksCountByBlockVersion(nextBlockMajorVersion), blockIndex, UseGenesis{false});
   auto commulativeDifficulties =
       getLastCumulativeDifficulties(currency.difficultyBlocksCountByBlockVersion(nextBlockMajorVersion), blockIndex, UseGenesis{false});
-  return currency.nextDifficulty(nextBlockMajorVersion, blockIndex, std::move(timestamps), std::move(commulativeDifficulties));
+  return currency.nextDifficulty(nextBlockMajorVersion, blockIndex+1, std::move(timestamps), std::move(commulativeDifficulties));
 }
 
 Difficulty DatabaseBlockchainCache::getCurrentCumulativeDifficulty() const {
