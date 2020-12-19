21af02226c0e9705bc797fa45b982cc49422b61a
Aiwe <maksymivv@gmail.com>
2020-12-19 18:39:46 +0200

    Correct typo in comment

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -1974,7 +1974,7 @@ bool RpcServer::onCheckReserveProof(const COMMAND_RPC_CHECK_RESERVE_PROOF::reque
     throw JsonRpc::JsonRpcError{ CORE_RPC_ERROR_CODE_WRONG_PARAM, "Failed to parse address " + req.address + '.' };
   }
 
-  // parse sugnature
+  // parse signature
   std::string decoded_data;
   uint64_t prefix;
   if (!Tools::Base58::decode_addr(req.signature, prefix, decoded_data) || prefix != CryptoNote::parameters::CRYPTONOTE_RESERVE_PROOF_BASE58_PREFIX) {
